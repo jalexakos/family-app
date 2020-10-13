36dcbc6f869753f5887c646b2b74e6dc11b20c79
jalexakos <64025205+jalexakos@users.noreply.github.com>
2020-10-13 19:44:23 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,8 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCy4-gTuQv9uU8UbxZp6ris6yeRfLh-f_U",
-    authDomain: "alexakos-family-app.firebaseapp.com",
-    databaseURL: "https://alexakos-family-app.firebaseio.com",
-    projectId: "alexakos-family-app",
-    storageBucket: "alexakos-family-app.appspot.com",
-    messagingSenderId: "449921798995",
-    appId: "1:449921798995:web:60b94b5722f6c440e7c7a6"
+    
+    {{ firebase API data Hidden }}
 
   }
 };
